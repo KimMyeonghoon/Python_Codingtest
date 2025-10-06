512447b834324c4e550a29a53902bc748bac0534
KimMyeonghoon <163105586+KimMyeonghoon@users.noreply.github.com>
2025-10-06 20:14:15 +0900

    Title: 윤년인가, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251006/윤년인가/is-leap-year.py
+++ 251006/윤년인가/is-leap-year.py
@@ -1,16 +1,14 @@
-Y = int(input())
 
-if Y % 100 == 0 :
-     if Y % 400 != 0:
-          print("false")
-     else :
-          if Y % 4 == 0:
-               print("true")
-          else : 
-               print("false") 
+y = int(input())
+
+
+if y % 4 == 0:
+	if y % 100 == 0:
+		if y % 400 == 0:
+			print("true")
+		else:
+			print("false")
+	else:
+		print("true")
 else:
-     if Y % 4 == 0 :
-          print("true")
-     else :
-          print("false")
-     
+	print("false")
