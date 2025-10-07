2790388799966625f66b24acfdc043fea29eac23
KimMyeonghoon <163105586+KimMyeonghoon@users.noreply.github.com>
2025-10-07 20:25:36 +0900

    Title: 짝수만 거꾸로 출력하기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251007/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.py
@@ -0,0 +1,11 @@
+N = int(input())
+M = map(int, input().split())
+H = []
+
+for i in M:
+    if i % 2 == 0:
+        H.append(i)
+    else : 
+        pass
+H.reverse()
+print(" ".join(map(str, H))) 
