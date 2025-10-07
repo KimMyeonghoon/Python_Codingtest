203e25f4c3590e0b98a87625176a99c4fd0c834b
KimMyeonghoon <163105586+KimMyeonghoon@users.noreply.github.com>
2025-10-07 18:43:13 +0900

    Title: 100점 도달하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251007/100점 도달하기/reach-100-point.py
+++ 251007/100점 도달하기/reach-100-point.py
@@ -1,6 +1,16 @@
-score = list(int(input()))
+score = (int(input()))
 num=[]
 
-for i in range (score):
-    num.append(i+=1)
-    print(num)
+for i in range (score, 101, 1):
+    if 90<= i < 101:
+        print("A", end=" ")
+    elif 80<= i < 90:
+        print("B", end=" ") 
+    elif 70<= i < 80:
+        print("C", end=" ") 
+    elif 60<= i < 70:
+        print("D", end=" ") 
+    else :
+        print("F", end=" ")
+    num.append(i)
+
