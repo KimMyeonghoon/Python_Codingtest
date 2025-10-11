8abd4fcedb9e00f6a845180f4458a6a210bb88af
KimMyeonghoon <163105586+KimMyeonghoon@users.noreply.github.com>
2025-10-12 00:16:22 +0900

    Title: 짝수의 합 n번 구하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251012/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.py
@@ -0,0 +1,16 @@
+n=int(input())
+for i in range(n):
+    a, b=map(int,(input().split()))
+    
+    c = 0
+    for i in range(a, b+1):
+        if i % 2 == 0 :
+            c+=i
+
+    print(c)
+
+
+
+
+    
+ 
