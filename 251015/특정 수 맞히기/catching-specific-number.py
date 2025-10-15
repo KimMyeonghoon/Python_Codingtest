3c883e3bc7e7b77264705471b6ff5b9e5d712362
KimMyeonghoon <163105586+KimMyeonghoon@users.noreply.github.com>
2025-10-15 22:02:46 +0900

    Title: 특정 수 맞히기, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251015/특정 수 맞히기/catching-specific-number.py
@@ -0,0 +1,13 @@
+
+count = 0 
+while count<3 :
+    num=int(input())
+    count+=1
+    if num < 25 :
+        print("Higher")
+    elif num > 25:
+        print("Lower")
+    else:
+        print("Good")
+        break
+    
