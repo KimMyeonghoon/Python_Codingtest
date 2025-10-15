3e849fa4c515e9eaac088cc24b4be306193393a0
KimMyeonghoon <163105586+KimMyeonghoon@users.noreply.github.com>
2025-10-15 22:26:04 +0900

    Title: 특정 수 맞히기, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 251015/특정 수 맞히기/catching-specific-number.py
+++ 251015/특정 수 맞히기/catching-specific-number.py
@@ -1,12 +1,9 @@
-count = 0 
-while count<3 :
-    num=int(input())
-    count+=1
-    if num < 25 :
+while True:
+    num = int(input())
+    if num < 25:
         print("Higher")
     elif num > 25:
         print("Lower")
     else:
         print("Good")
-
-    
+        break
